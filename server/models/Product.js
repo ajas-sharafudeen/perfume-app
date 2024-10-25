59fbfa14baccc74d08ccf6d84d35630d69362d5d
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-10-25 23:07:06 +0530

    created models

---
+++ server/models/Product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+  title: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  categories: { type: Array },
+  size: { type: String },
+  color: { type: String },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
