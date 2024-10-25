f20a75baca2cfc1dd9709744210e4c8caae40fdf
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-10-25 20:19:55 +0530

    setting up server

--- server/server.js
+++ server/server.js
@@ -4,17 +4,18 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
+const userRoute = require('./routes/user');
 
 require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
 
 // Creates a database connection
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log('MongoDB connected'))
   .catch((error) => console.log(error));
-
-app.use(cors());
-app.use(express.json());
 
 //cors configuration
 app.use(
@@ -24,8 +25,7 @@ app.use(
   })
 );
 
-app.use(cookieParser());
-app.use(express.json());
+app.use('/api/user', userRoute);
 
 // Define routes and middleware
 app.listen(PORT, () => {
