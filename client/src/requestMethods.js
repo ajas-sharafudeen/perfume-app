c7d3bd604e19529e65afa5204f5e206408470a47
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-11-03 20:54:33 +0530

    fixed typo: 'header -> headers'. Token access from localStorage

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/';
-const TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MWY1OWI5ZTFjODZkMDJlNjUyNDEyOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTczMDM2NzMyMiwiZXhwIjoxNzMwNjI2NTIyfQ.bHTwpzNCx95op43iljQ6ktIqSdj6ofHA8fhgJHMDZII';
 
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
 
-// const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
-// const currentUser = user && JSON.parse(user).currentUser;
-// const TOKEN = currentUser?.accessToken;
+const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -18,5 +16,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });
