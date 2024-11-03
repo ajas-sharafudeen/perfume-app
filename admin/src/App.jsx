97fc46df15e0d07260abba9453cb64aabc9835c6
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-11-03 20:52:57 +0530

    admin page: IN PROGRESS

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,33 +1,125 @@
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
 import ProductList from './pages/productList/ProductList';
 import Product from './pages/product/Product';
 import NewProduct from './pages/newProduct/NewProduct';
+import Login from './pages/login/Login';
 import './App.css';
 
 function App() {
+  const admin = JSON.parse(
+    JSON.parse(localStorage.getItem('persist:root')).user
+  ).currentUser.isAdmin;
   return (
+    // <Router>
+    //   <Routes>
+    //     <Route path="/login" element={<Login />} />
+    //     {admin && (
+    //       <>
+    //         <Topbar />
+    //         <div className="container">
+    //           <Sidebar />
+    //           <Route path="/" element={<Home />} />
+    //           <Route path="/users" element={<UserList />} />
+    //           <Route path="/user/:userId" element={<User />} />
+    //           <Route path="/newUser" element={<NewUser />} />
+    //           <Route path="/products" element={<ProductList />} />
+    //           <Route path="/product/:productId" element={<Product />} />
+    //           <Route path="/newProduct" element={<NewProduct />} />
+    //         </div>
+    //       </>
+    //     )}
+    //   </Routes>
+    // </Router>
     <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
+      {admin && (
+        <>
+          <Topbar />
+          <div className="container">
+            <Sidebar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/users" element={<UserList />} />
+              <Route path="/user/:userId" element={<User />} />
+              <Route path="/newUser" element={<NewUser />} />
+              <Route path="/products" element={<ProductList />} />
+              <Route path="/product/:productId" element={<Product />} />
+              <Route path="/newproduct" element={<NewProduct />} />
+            </Routes>
+          </div>
+        </>
+      )}
+      {!admin && (
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="/user/:userId" element={<User />} />
-          <Route path="/newUser" element={<NewUser />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/newProduct" element={<NewProduct />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
-      </div>
+      )}
     </Router>
   );
 }
 
 export default App;
+
+// import Sidebar from './components/sidebar/Sidebar';
+// import Topbar from './components/topbar/Topbar';
+// import Home from './pages/home/Home';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import UserList from './pages/userList/UserList';
+// import User from './pages/user/User';
+// import NewUser from './pages/newUser/NewUser';
+// import ProductList from './pages/productList/ProductList';
+// import Product from './pages/product/Product';
+// import NewProduct from './pages/newProduct/NewProduct';
+// import Login from './pages/login/Login';
+// import './App.css';
+
+// function App() {
+//   const admin = JSON.parse(
+//     JSON.parse(localStorage.getItem('persist:root')).user
+//   ).currentUser.isAdmin;
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/login" element={<Login />} />
+//         {admin && (
+//           <>
+//             <Route
+//               path="*"
+//               element={
+//                 <>
+//                   <Topbar />
+//                   <div className="container">
+//                     <Sidebar />
+//                     <Routes>
+//                       <Route path="/" element={<Home />} />
+//                       <Route path="/users" element={<UserList />} />
+//                       <Route path="/user/:userId" element={<User />} />
+//                       <Route path="/newUser" element={<NewUser />} />
+//                       <Route path="/products" element={<ProductList />} />
+//                       <Route path="/product/:productId" element={<Product />} />
+//                       <Route path="/newProduct" element={<NewProduct />} />
+//                     </Routes>
+//                   </div>
+//                 </>
+//               }
+//             />
+//           </>
+//         )}
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
