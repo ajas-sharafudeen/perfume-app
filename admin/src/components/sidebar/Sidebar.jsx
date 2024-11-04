c23b5233a330e6499b4fe2cf2be3e469265d56d3
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-11-04 20:20:14 +0530

    alpha version completed

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -23,7 +23,7 @@ function Sidebar() {
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
             <Link to="/" className="link">
-              <li className="sidebarListItem active">
+              <li className="sidebarListItem">
                 <LineStyle className="sidebarIcon" />
                 Home
               </li>
--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -1,30 +1,129 @@
+import { useState } from 'react';
+import { addProduct } from '../../redux/apiCalls';
+import { useDispatch } from 'react-redux';
 import './newProduct.css';
 
 function NewProduct() {
+  const [inputs, setInputs] = useState({});
+  const [file, setFile] = useState(null);
+  const [cat, setCat] = useState([]);
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const [cloudinaryUrl, setCloudinaryUrl] = useState(null);
+  const dispatch = useDispatch();
+
+  const cloudinaryName = import.meta.env.VITE_CLOUD_NAME;
+  const cloudinaryPreset = import.meta.env.VITE_CLOUD_PRESET;
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleCat = (e) => {
+    setCat(e.target.value.split(','));
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+    setPreviewUrl(URL.createObjectURL(selectedFile));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (!file) {
+      console.error('No file selected');
+      return;
+    }
+
+    // Upload to Cloudinary
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', cloudinaryPreset);
+
+    try {
+      const response = await fetch(
+        `https://api.cloudinary.com/v1_1/${cloudinaryName}/image/upload`,
+        {
+          method: 'POST',
+          body: formData,
+        }
+      );
+
+      const data = await response.json();
+      const downloadURL = data.secure_url;
+      setCloudinaryUrl(downloadURL);
+
+      const product = { ...inputs, img: downloadURL, categories: cat };
+      addProduct(product, dispatch);
+    } catch (error) {
+      console.error('Error uploading to Cloudinary:', error);
+    }
+  };
+
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input
+            type="file"
+            id="file"
+            accept="image/jpeg, image/png, image/jpg"
+            onChange={handleFileChange}
+          />
+          {previewUrl && (
+            <img
+              src={previewUrl}
+              alt="Local preview"
+              style={{ width: '200px', height: 'auto', marginTop: '10px' }}
+            />
+          )}
         </div>
         <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <label>Title</label>
+          <input
+            name="title"
+            type="text"
+            placeholder="Apple Airpods"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Description</label>
+          <input
+            name="desc"
+            type="text"
+            placeholder="description..."
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Price</label>
+          <input
+            name="price"
+            type="number"
+            placeholder="100"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Categories</label>
+          <input type="text" placeholder="men, women" onChange={handleCat} />
         </div>
         <div className="addProductItem">
           <label>Stock</label>
-          <input type="text" placeholder="123" />
-        </div>
-        <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
+          <select name="inStock" onChange={handleChange}>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+        <button onClick={handleClick} className="addProductButton">
+          Create
+        </button>
       </form>
     </div>
   );
